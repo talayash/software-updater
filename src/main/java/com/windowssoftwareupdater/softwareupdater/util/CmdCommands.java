2f68e0b0049de28a706337547e26718b37194bd6
Tal Ayash <talayash@gmail.com>
2024-08-18 19:04:15 +0300

    Task #1 - Add CMD commands

---
+++ src/main/java/com/windowssoftwareupdater/softwareupdater/util/CmdCommands.java
@@ -0,0 +1,20 @@
+package com.windowssoftwareupdater.softwareupdater.util;
+
+public class CmdCommands {
+    // Attributes
+    public static final String WINGET_UPGRADE = "winget upgrade";
+    public static final String WINGET_UPGRADE_ALL = "winget upgrade --all";
+    public static final String VERSION = "--version";
+
+    /**
+     * Updates a specific version of a software using the Windows command line utility WINGET.
+     * Example: "winget upgrade discord --version 0.15.2"
+     *
+     * @param softwareName the name of the software to update
+     * @param targetVersion the target version to update to
+     * @return the command to execute for updating the specific version of the software
+     */
+    public static String updateSpecificVersion(String softwareName, String targetVersion) {
+        return WINGET_UPGRADE + " " + softwareName + VERSION + " " + targetVersion;
+    }
+}
